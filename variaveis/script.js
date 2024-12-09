fb8a15983f432f79f966148116edc23249549724
Fernando Aguiar da Costa Morais <fernando2015.aguiar@gmail.com>
2024-12-09 20:09:35 -0300

    javascript - para iniciantes

---
+++ variaveis/script.js
@@ -0,0 +1,11 @@
+var nome = "Fernando";
+var idade = 31;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var precoTotal = totalComprado * preco;
+
+console.log(precoTotal);
